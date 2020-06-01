36d0f973fad7743f2642d1f7e91e6af52b099012
Piotr Trocki <piotr.trocki@callstack.com>
2020-06-01 18:22:18 +0200

    Add additinal layer responsible for catching events

--- android/src/main/java/com/reactnativecommunity/viewpager/FragmentAdapter.java
+++ android/src/main/java/com/reactnativecommunity/viewpager/FragmentAdapter.java
@@ -1,58 +1,97 @@
 package com.reactnativecommunity.viewpager;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.reactnative.community.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class FragmentAdapter extends FragmentStateAdapter {
 
-    public FragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
-        super(fragmentActivity);
+public class FragmentAdapter extends RecyclerView.Adapter<FragmentAdapter.ReactNativeViewHolder> {
+
+    private ArrayList<View> reactChildrenViews = new ArrayList();
+
+    class RNL extends FrameLayout {
+
+        public RNL(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+            super.onLayout(changed,l,t,r,b);
+        }
+
+
+        @Override
+        public boolean dispatchTouchEvent(MotionEvent ev) {
+//            Log.d("aaa", "dispatchTouchEvent: " + String.valueOf(super.dispatchTouchEvent(ev)));
+            return false;
+        }
+
     }
-
-    private ArrayList<ViewPagerFragment> children = new ArrayList<>();
 
     @NonNull
     @Override
-    public Fragment createFragment(int position) {
-        return children.get(position);
+    public ReactNativeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RNL rootView = new RNL(parent.getContext());
+        rootView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        rootView.setBackgroundColor(Color.parseColor("#32a852"));
+        return new ReactNativeViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReactNativeViewHolder holder, int position) {
+        holder.bind(reactChildrenViews.get(position));
+
     }
 
     @Override
     public int getItemCount() {
-        return children.size();
+        return reactChildrenViews.size();
     }
 
-    public void addFragment(View child, int index) {
-        children.add(ViewPagerFragment.newInstance(child.getId()));
-        notifyDataSetChanged();
-    }
-
-
-    public void removeFragment(View child) {
-        for (int i = 0; i < children.size(); i++) {
-            Fragment fragment = children.get(i);
-            if (fragment.getId() == child.getId()) {
-                children.remove(i);
-                notifyItemRemoved(i);
-                return;
-            }
-        }
-    }
-
-    public void removeFragmentAt(int index) {
-        children.remove(index);
-        notifyItemRemoved(index);
+    public void addFragment(View reactNativeChild, int index) {
+        reactChildrenViews.add(index, reactNativeChild);
     }
 
     public View getChildAt(int index) {
-        return children.get(index).getView();
+        return reactChildrenViews.get(index);
+    }
+
+    public void removeFragment(View view) {
+
+    }
+
+    public void removeFragmentAt(int index) {
+
+    }
+
+    class ReactNativeViewHolder extends RecyclerView.ViewHolder {
+
+        public ReactNativeViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(View view) {
+            RNL rootview = (RNL) itemView;
+            rootview.addView(view);
+        }
     }
 }
--- android/src/main/java/com/reactnativecommunity/viewpager/ReactViewPagerManager.java
+++ android/src/main/java/com/reactnativecommunity/viewpager/ReactViewPagerManager.java
@@ -7,12 +7,21 @@
 
 package com.reactnativecommunity.viewpager;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
 import android.util.SparseArray;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
 
@@ -38,14 +47,13 @@ import static com.reactnative.community.viewpager2.widget.ViewPager2.SCROLL_STAT
 import static com.reactnative.community.viewpager2.widget.ViewPager2.SCROLL_STATE_IDLE;
 import static com.reactnative.community.viewpager2.widget.ViewPager2.SCROLL_STATE_SETTLING;
 
-public class ReactViewPagerManager extends ViewGroupManager<ViewPager2> {
+public class ReactViewPagerManager extends ViewGroupManager<ReactViewPagerManager.RNL> {
 
     private static final String REACT_CLASS = "RNCViewPager";
     private static final int COMMAND_SET_PAGE = 1;
     private static final int COMMAND_SET_PAGE_WITHOUT_ANIMATION = 2;
     private static final int COMMAND_SET_SCROLL_ENABLED = 3;
     private EventDispatcher eventDispatcher;
-    static SparseArray<View> reactChildrenViews = new SparseArray<>();
 
     @NonNull
     @Override
@@ -53,11 +61,36 @@ public class ReactViewPagerManager extends ViewGroupManager<ViewPager2> {
         return REACT_CLASS;
     }
 
+    class RNL extends FrameLayout {
+
+        public RNL(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+            super.onLayout(changed, l, t, r, b);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            Log.d("", "onTouchEvent: " + MotionEvent.actionToString(event.getAction()));
+            return true;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            return true;
+        }
+    }
+
     @NonNull
     @Override
-    protected ViewPager2 createViewInstance(@NonNull ThemedReactContext reactContext) {
+    protected RNL createViewInstance(@NonNull ThemedReactContext reactContext) {
         final ViewPager2 vp = new ViewPager2(reactContext);
-        FragmentAdapter adapter = new FragmentAdapter((FragmentActivity) reactContext.getCurrentActivity());
+        FragmentAdapter adapter = new FragmentAdapter();
         vp.setAdapter(adapter);
         eventDispatcher = reactContext.getNativeModule(UIManagerModule.class).getEventDispatcher();
         vp.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
@@ -96,39 +129,50 @@ public class ReactViewPagerManager extends ViewGroupManager<ViewPager2> {
                         new PageScrollStateChangedEvent(vp.getId(), pageScrollState));
             }
         });
-        return vp;
+        RNL viewG = new RNL(reactContext);
+        viewG.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        vp.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        vp.setBackgroundColor(Color.parseColor("#eb34c0"));
+        vp.setTag(86768);
+        viewG.addView(vp);
+
+        View layer = new View(reactContext);
+        layer.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layer.setBackgroundColor(Color.parseColor("#fcba03"));
+        viewG.addView(layer,1);
+        return viewG;
     }
 
     @Override
-    public void addView(ViewPager2 parent, View child, int index) {
+    public void addView(RNL parent, View child, int index) {
         if (child == null) {
             return;
         }
-        reactChildrenViews.put(child.getId(), child);
-        ((FragmentAdapter) parent.getAdapter()).addFragment(child, index);
+        ((FragmentAdapter)((ViewPager2)parent.findViewWithTag(86768)).getAdapter()).addFragment(child, index);
     }
 
     @Override
-    public int getChildCount(ViewPager2 parent) {
-        return parent.getAdapter().getItemCount();
+    public int getChildCount(RNL parent) {
+        if(parent.findViewWithTag(86768) == null){
+            return 0;
+        }
+        return ((FragmentAdapter)((ViewPager2)parent.findViewWithTag(86768)).getAdapter()).getItemCount();
     }
 
 
     @Override
-    public View getChildAt(ViewPager2 parent, int index) {
-        return ((FragmentAdapter) parent.getAdapter()).getChildAt(index);
+    public View getChildAt(RNL parent, int index) {
+        return    ((FragmentAdapter)((ViewPager2)parent.findViewWithTag(86768)).getAdapter()).getChildAt(index);
     }
 
     @Override
-    public void removeView(ViewPager2 parent, View view) {
-        reactChildrenViews.remove(view.getId());
-        ((FragmentAdapter) parent.getAdapter()).removeFragment(view);
+    public void removeView(RNL parent, View view) {
+        ((FragmentAdapter)((ViewPager2)parent.findViewWithTag(86768)).getAdapter()).removeFragment(view);
     }
 
     @Override
-    public void removeViewAt(ViewPager2 parent, int index) {
-        reactChildrenViews.removeAt(index);
-        ((FragmentAdapter) parent.getAdapter()).removeFragmentAt(index);
+    public void removeViewAt(RNL parent, int index) {
+        ((FragmentAdapter)((ViewPager2)parent.findViewWithTag(86768)).getAdapter()).removeFragmentAt(index);
     }
 
     @Override
@@ -138,13 +182,13 @@ public class ReactViewPagerManager extends ViewGroupManager<ViewPager2> {
 
 
     @ReactProp(name = "scrollEnabled", defaultBoolean = true)
-    public void setScrollEnabled(ViewPager2 viewPager, boolean value) {
-        viewPager.setUserInputEnabled(value);
+    public void setScrollEnabled(RNL viewPager, boolean value) {
+//        viewPager.setUserInputEnabled(value);
     }
 
     @ReactProp(name = "orientation")
-    public void setOrientation(ViewPager2 viewPager, String value) {
-        viewPager.setOrientation(value.equals("vertical") ? ViewPager2.ORIENTATION_VERTICAL : ORIENTATION_HORIZONTAL);
+    public void setOrientation(RNL viewPager, String value) {
+//        viewPager.setOrientation(value.equals("vertical") ? ViewPager2.ORIENTATION_VERTICAL : ORIENTATION_HORIZONTAL);
     }
 
 
@@ -169,24 +213,24 @@ public class ReactViewPagerManager extends ViewGroupManager<ViewPager2> {
     }
 
     @Override
-    public void receiveCommand(@NonNull final ViewPager2 root, int commandId, @Nullable final ReadableArray args) {
+    public void receiveCommand(@NonNull final RNL root, int commandId, @Nullable final ReadableArray args) {
         super.receiveCommand(root, commandId, args);
         Assertions.assertNotNull(root);
         Assertions.assertNotNull(args);
         switch (commandId) {
             case COMMAND_SET_PAGE: {
-                root.setCurrentItem(args.getInt(0), true);
+//                root.setCurrentItem(args.getInt(0), true);
                 eventDispatcher.dispatchEvent(new PageSelectedEvent(root.getId(), args.getInt(0)));
                 return;
 
             }
             case COMMAND_SET_PAGE_WITHOUT_ANIMATION: {
-                root.setCurrentItem(args.getInt(0), false);
+//                root.setCurrentItem(args.getInt(0), false);
                 eventDispatcher.dispatchEvent(new PageSelectedEvent(root.getId(), args.getInt(0)));
                 return;
             }
             case COMMAND_SET_SCROLL_ENABLED: {
-                root.setUserInputEnabled(args.getBoolean(0));
+//                root.setUserInputEnabled(args.getBoolean(0));
                 return;
             }
             default:
@@ -199,9 +243,9 @@ public class ReactViewPagerManager extends ViewGroupManager<ViewPager2> {
 
 
     @ReactProp(name = "pageMargin", defaultFloat = 0)
-    public void setPageMargin(ViewPager2 pager, float margin) {
-        int pageMargin = (int) PixelUtil.toPixelFromDIP(margin);
-        pager.setPadding(pageMargin, pageMargin, pageMargin, pageMargin);
+    public void setPageMargin(RNL pager, float margin) {
+//        int pageMargin = (int) PixelUtil.toPixelFromDIP(margin);
+//        pager.setPadding(pageMargin, pageMargin, pageMargin, pageMargin);
     }
 
 }
